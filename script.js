71b5eec62161a79e88ca1fd1a0961e83b956020f
Shreyas2545 <shreyashadawale25@gmail.com>
2025-08-30 20:33:01 +0530

    email.js configuration

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail() {
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+  emailjs.send("service_buxrfhw","template_u2eqrbn",parms).then(alert("Email sent successfully!!!"))
+}
